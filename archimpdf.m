0d250185144b22d772c5202b60abec23457840c3
Martin Scavnicky <devnull@localhost>
2012-10-26 11:18:13 +0100

    implemented archimpdf

--- archimpdf.m
+++ archimpdf.m
@@ -1,24 +1,15 @@
 function [ Y ] = archimpdf( family, U, alpha )
-%ARCHIMPDF
-% McNeil & Neslehova
+%ARCHIMPDF Probability density function for multivariate archimedean copula.
+% 
+% TODO: 
+% - add source McNeil & Neslehova
+% - validation
 
-% Matrix dimension to compute derivative
+% Acquire copula dimension for derivative
 d = size(U, 2);
+% Evaluate equation [1]
+N = archimndiff( family, d, sum(archiminv( family, U, alpha ), 2), alpha );
+D = prod( archimdiff( family, archiminv( family, U, alpha ), alpha ), 2 );
+Y = N ./ D;
 
-% Declare symbols in symbolic expression
-syms x p
-
-% Select generator
-switch family
-    case 'frank'
-        g = (-1/p) * log( 1 - ( 1 - exp(-p) ) .* exp(-x) );
-    case 'gumbel'
-        g = exp( -x ^ (1/p) );       
-    case 'clayton'
-        g = ( 1 + p*X ) ^ (-1/p);
 end
-
-% Differentiate generator dimension times
-
-
-
