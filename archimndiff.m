7bb1fe6313d2db0f5790c27b28cf8669b83251a6
Martin Scavnicky <devnull@localhost>
2012-10-26 11:07:28 +0100

    implemented archimndiff function

---
+++ archimndiff.m
@@ -0,0 +1,26 @@
+function [ Y ] = archimndiff( family, n, X, p )
+%ARCHIMNDIFF Computes n-th derivate of Archimedean generator and evaluates.
+% Derivation is computed analytically. Supports Frank, Gumbel and Clayton 
+% copulas.
+
+% Declare symbols
+syms x p
+% Define symbolic version of generator
+switch family
+    case 'frank'
+        f = (-1/p) * log( 1 - ( 1 - exp(-p) ) * exp(-x) );        
+    case 'clayton'
+        f = (1 + p*x) ^ (-1/p);
+    case 'gumbel'
+        f = exp(-x ^ (1/p));
+end
+
+% Analytically compute n-th derivation
+fn = diff(f, n);
+% Convert derivation to Matlab function
+ndiff = matlabFunction(fn);
+% Compute function values
+Y = ndiff(X, p);
+        
+end
+
