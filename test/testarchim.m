c92b0a116bf651eb6ed8eb3c3e524809b2d7e2e8
Martin Scavnicky <devnull@localhost>
2012-10-27 15:19:03 +0100

    optimized uniform function

--- test/testarchim.m
+++ test/testarchim.m
@@ -28,22 +28,21 @@ function testArchimpdf
     
 function testArchimfitGumbel
 % Tests 2 dimensional case of archimfit on copulafit
-    X = colfun(@uniform, normrnd(0, 1, 10000, 2));
+    X = uniform(normrnd(0, 1, 10000, 2));
     [ alpha1 ] = archimfit('gumbel', X);
     [ alpha2 ] = copulafit('gumbel', X);
-    assertVectorsAlmostEqual(alpha1, alpha2, 'absolute', 0.0001);
-    
+    assertVectorsAlmostEqual(alpha1, alpha2, 'absolute', 0.0001);    
     
 function testArchimfitFrank
 % Tests 2 dimensional case of archimfit on copulafit
-    X = colfun(@uniform, normrnd(0, 1, 10000, 2));
+    X = uniform(normrnd(0, 1, 10000, 2));
     [ alpha1 ] = archimfit('frank', X);
     [ alpha2 ] = copulafit('frank', X);
     assertVectorsAlmostEqual(alpha1, alpha2, 'absolute', 0.0001);
     
 function testArchimfitClayton
 % Tests 2 dimensional case of archimfit on copulafit
-    X = colfun(@uniform, normrnd(0, 1, 10000, 2));
+    X = uniform(normrnd(0, 1, 10000, 2));
     [ alpha1 ] = archimfit('clayton', X);
     [ alpha2 ] = copulafit('clayton', X);
     assertVectorsAlmostEqual(alpha1, alpha2, 'absolute', 0.0001);
--- uniform.m
+++ uniform.m
@@ -1,24 +1,33 @@
-function [ f ] = uniform( x )
-%UNIFORM Converts sample vector into uniform variates.
-%
-% Works with both row and column vectors. Does not work with matrices.
-% Based on empirical CDF function described in Berg, Bakken (2006).
+function [ U ] = uniform( X, dim )
+%UNIFORM Converts matrix to uniform variates in specified dimension.
+%   Based on empirical CDF function described in Berg, Bakken (2006).
+%   Function ISMEMBC2 is uncomented an returns last position of element
+%   array.
 
-[m, n] = size(x);
+if nargin < 2
+    dim = 1;
+end
 
-if (m == 1)
-    f = zeros(1, n);
-    for i=1:n
-        f(1, i) = sum(x(:) <= x(i)) / (n + 1);
-    end
-elseif (n == 1)
-    f = zeros(m, 1);
-    for i=1:m
-        f(i, 1) = sum(x(:) <= x(i)) / (m + 1);
+[m, n] = size(X);
+U = zeros(m, n);
+
+if dim == 1 % uniform cols
+    for i=1:n % for each column
+        S = sort(X(:,i));
+        for j=1:m
+            U(j, i) = ismembc2(X(j,i), S) / (m + 1);
+        end
+    end     
+elseif dim == 2 % uniform rows
+    for i=1:m % for each row
+        S = sort(X(i,:));
+        for j=1:n
+            U(i, j) = ismembc2(X(i,j), S) / (n + 1);
+        end
     end    
 else
-    error 'Not a vector.'
+    error 'Incorrect dimension.'
 end
-   
+
 end
 
