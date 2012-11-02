801e4e12425a175a50068049946c95216f99faff
Martin Scavnicky <devnull@localhost>
2012-11-02 20:49:00 +0100

    implemented symbolic version of archiminv

---
+++ archiminvsym.m
@@ -0,0 +1,15 @@
+function [ f ] = archiminvsym( family, x, p )
+%ARCHIMINVSYM Symbolic represenatation of inverse of archimedean copula 
+%generator.
+
+switch family
+    case 'frank'
+        f = -log( ( exp(-p * x) - 1 ) / ( exp(-p) - 1 ) );
+    case 'clayton'
+        f = ( 1 / p ) * ( x .^ -p - 1 );
+    case 'gumbel'
+        f = ( -log(x) ) .^ p;
+end
+
+end
+
