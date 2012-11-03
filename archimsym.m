fa1b5c8b35488517dc8ccdbc4a72496a0336fe32
Martin Scavnicky <devnull@localhost>
2012-11-03 21:09:16 +0100

    implemented and tested symbolic representation of copula function

---
+++ archimsym.m
@@ -0,0 +1,17 @@
+function f = archimsym( family, symbols, alpha )
+%ARCHIMSYM Produces symbolic expression of given Archimedean copula.
+%   Given dimension produces symbolic expression n-dimensional Archimedean
+%   copula using provided symbols for variables and alpha for parameter symbol.
+
+% Sum together all inverse generators
+f = sym(0);
+for i=1:length(symbols)
+    s = symbols(i);
+    f = f + archiminvsym(family, s, alpha);
+end
+
+% Appply generator over the sum
+f = archimgensym(family, f, alpha);
+
+end
+
---
+++ test/test_archimsym.m
@@ -0,0 +1,14 @@
+%#ok<*DEFNU>
+%#ok<*STOUT>
+
+function test_suite = test_archimsym
+initTestSuite;
+
+function test_archimsym_gumbel
+% Test generating and evaluating symbolic expression of Gumbel copula    
+    syms u1 u2 a;
+    f = archimsym('gumbel', [u1 u2], a);
+    g = matlabFunction(f, 'vars', [u1 u2 a]);
+    
+    X = [0.5 0.4; 0.7 0.2; 0.4 0.1];
+    assertVectorsAlmostEqual(g(X(:,1), X(:,2), 1.5), copulacdf('gumbel', X, 1.5));
