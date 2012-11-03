ae7f1b6c2327f6246e490fe475ccac3251b94210
Martin Scavnicky <devnull@localhost>
2012-11-03 09:52:36 +0100

    tested uniform function

---
+++ test/testuniform.m
@@ -0,0 +1,21 @@
+%#ok<*DEFNU>
+%#ok<*STOUT>
+
+function test_suite = testuniform
+initTestSuite;
+
+function testUniformRowVector
+    x = [1 2 3 3 4 5 1];
+    expected = [0.25 0.375 0.625 0.625 0.75 0.875 0.25];
+    assertVectorsAlmostEqual(expected, uniform(x, 2));
+    
+function testUniformColumnVector
+    x = [1; 2; 3; 3; 4; 5; 5];
+    expected = [0.125; 0.25; 0.5; 0.5; 0.625; 0.875; 0.875];
+    assertVectorsAlmostEqual(expected, uniform(x, 1));
+    
+function testUniformMatrix
+    X = [1 2 3; 4 5 6];
+    expected = [0.3333 0.3333 0.3333; 0.6666 0.6666 0.6666];
+    assertVectorsAlmostEqual(expected, uniform(X), 'absolute', 0.001);
+
