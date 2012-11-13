6e21dc7875c912c18d0034503bbd183cce81037d
Martin Scavnicky <devnull@localhost>
2012-11-13 23:16:12 +0000

    removed unused functions

--- colfun.m
+++
@@ -1,5 +0,0 @@
-function [ Y ] = colfun( fun, X )
-%COLFUN Applies function to each column of X
-Y = cell2mat(cellfun(fun, num2cell(X, 1), 'UniformOutput', false));
-end
-
--- rowfun.m
+++
@@ -1,5 +0,0 @@
-function [ Y ] = rowfun( fun, X )
-%ROWFUN Applies function to each row of X
-Y = cell2mat(cellfun(fun, num2cell(X, 2), 'UniformOutput', false));
-end
-
