58e5f86b0877f63820a8957d6917e65b1b546729
Martin Scavnicky <devnull@localhost>
2012-11-14 11:17:25 +0000

    implemtend debug print function

---
+++ dbg.m
@@ -0,0 +1,13 @@
+function dbg( varargin )
+%DBG Print debug output.
+%   Prints debug output to console only if showDebug is defined. Use the
+%   same arguments that you would use for fprintf functions.
+
+global showDebug;
+
+if showDebug
+   fprintf(varargin(:));
+end
+
+end
+
