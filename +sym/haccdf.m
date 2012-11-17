56f73911170cabf5a45e8e5443f83acec24ab2f0
Martin Scavnicky <devnull@localhost>
2012-11-17 17:54:11 +0000

    moved hacsym into separate file

---
+++ +sym/haccdf.m
@@ -0,0 +1,25 @@
+function [ f ] = haccdf( family, hac )
+%HACSYM Symbolic representation of HAC of given family and structure.
+
+% Dimensions of top-level copula in hac structure
+d = length(hac) - 1;
+% Cell array where arguments for this level copula are collected
+arguments = {};
+
+for i=1:d
+    % Perform recursion if element of structure is another copula
+    if iscell(hac{i})
+        arguments{i} = hacsym(family, hac{i});
+    else
+        % Introdcue new symbol
+        arguments{i} = sym(sprintf('u%d', hac{i}));
+    end    
+end
+% Retrieve alpha from hac structure and convert it to decimal symbol
+% Conversion to decimal symbol is necessary otherwise expression will end
+% up with humongous fractions.
+alpha = sym(hac{end}, 'd');
+% Compose copula function from arguments and numerical alpha
+f = sym.archimcdf(family, arguments, alpha);
+
+end
--- hacpdf.m
+++ hacpdf.m
@@ -4,7 +4,7 @@ function [ Y ] = hacpdf( family, U, hac )
 %   HAC.
 
 % Express analytical version of hierarchical copula function
-f = hacsym(family, hac);
+f = sym.hacpdf(family, hac);
 % Get all arguments that we will use to differentiate copula function
 % Even though not documented symvar sorts retrieved variables
 args = symvar(f);
@@ -13,33 +13,8 @@ for j=1:length(args)
     f = diff(f, args(j));
 end
 
+% Convert to Matlab function and evaluate
 fn = matlabFunction(f, 'vars', {args});
 Y = fn(U);
-
-end
-
-function [ f ] = hacsym( family, hac )
-%HACSYM Finds symbolic representation of HAC of given family.
-
-% Dimensions of top-level copula in hac structure
-d = length(hac) - 1;
-% Cell array where arguments for this level copula are collected
-arguments = {};
-
-for i=1:d
-    % Perform recursion if element of structure is another copula
-    if iscell(hac{i})
-        arguments{i} = hacsym(family, hac{i});
-    else
-        % Introdcue new symbol
-        arguments{i} = sym(sprintf('u%d', hac{i}));
-    end    
-end
-% Retrieve alpha from hac structure and convert it to decimal symbol
-% Conversion to decimal symbol is necessary otherwise expression will end
-% up with humongous fractions.
-alpha = sym(hac{end}, 'd');
-% Compose copula function from arguments and numerical alpha
-f = sym.archimcdf(family, arguments, alpha);
 
 end
