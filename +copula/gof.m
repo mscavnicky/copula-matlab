78eda81e0f316fad035daaa25a0c91b592f72aa8
Martin Scavnicky <devnull@localhost>
2012-11-22 23:56:07 +0000

    implemented SnC goodness-of-fit test

--- +copula/gof.m
+++ +copula/gof.m
@@ -23,6 +23,23 @@ case 'snc'
     
     p = mean(T > t);
     h = p > 0.05;
+case 'snb'
+    % Compute statistics value for original dataset
+    t = snc( family, U, copulaparams );
+    % Boostraped statistics
+    T = zeros(N, 1);
+    for i=1:N
+       fprintf('GOF Boostrap %d...\n', i);
+       % Simulate fitted copula
+       V = uniform(copula.rnd(family, n, d, copulaparams));
+       % Fit simulated data
+       copulaparams = copula.fit(family, U);
+       % Get another bootstrapped statistics
+       T(i) = snb( family, V, copulaparams ); 
+    end
+    
+    p = mean(T > t);
+    h = p > 0.05;
 otherwise
     error('Method %s not recognized', method);
 end
@@ -35,5 +52,26 @@ function [ T ] = snc( family, U, copulaparams )
     E = copula.pit(family, U, copulaparams);
     % Compute test statistic
     T = sum((ecopula(E) - prod(E, 2)) .^ 2);
+end
+
+function [ T ] = snb( family, U, copulaparams )
+
+[n, d] = size(U);
+
+E = copula.pit(family, U, copulaparams);
+
+t1 = n / 3^d;
+t2 = sum(prod(1 - E.^2, 2), 1) / 2^(d-1);
+
+t3 = 0;
+for i=1:n
+   for j=1:n
+      t3 = t3 + prod(1 - max(E(i,:), E(j,:)), 2);
+   end
+end
+t3 = t3 / n;
+
+T = t1 - t2 + t3;
+
 end
 
