d23f378633828974e759984a7840e61904080699
Martin Scavnicky <devnull@localhost>
2012-11-23 09:56:45 +0000

    tested ecopula with non-unique ranks

--- test/test_ecopula.m
+++ test/test_ecopula.m
@@ -14,6 +14,11 @@ function testEmpiricalCopulaIn2D_2
     C = [0.2; 0.6; 0.6; 0.4];
     assertVectorsAlmostEqual(C, ecopula(uniform(X)));
     
+function testEmpiricalCopulaIn2DWithEqualElements
+    X = [1, 1; 2, 2; 1, 1; 2, 2];
+    C = [0.4; 0.8; 0.4; 0.8];
+    assertVectorsAlmostEqual(C, ecopula(uniform(X)));    
+    
 function testEmpiricalCopulaIn3D
     X = [0.1 0.2 0.3; 0.9 0.4 0.5; 0.5 0.5 0.8; 0.2 0.4 0.7];
     C = [0.2; 0.4; 0.6; 0.4];
