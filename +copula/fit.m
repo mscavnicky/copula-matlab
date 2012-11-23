65f52b0ec139c43a40c7065d63699aac2e25c17a
Martin Scavnicky <devnull@localhost>
2012-11-23 14:38:37 +0000

    enriched copula fit structure with more fields

--- +copula/fit.m
+++ +copula/fit.m
@@ -1,20 +1,33 @@
 function [ copulaparams ] = fit( family, U )
 %COPULA.FIT Fit data to copula.
 
+d = size(U, 2);
+
+copulaparams.family = family;
+copulaparams.dim = d;
+
 switch family
     case {'gaussian'}
         rho = copulafit(family, U);
         copulaparams.rho = rho;
+        
+        copulaparams.numParams = d*(d-1) / 2;
     case {'t'}
         [rho, nu] = copulafit(family, U);
         copulaparams.rho = rho;
         copulaparams.nu = nu;
+        
+        copulaparams.numParams = 1 + d*(d-1) / 2;
     case {'clayton', 'gumbel', 'frank'}
         alpha = archim.fit(family, U);
         copulaparams.alpha = alpha;
+        
+        copulaparams.numParams = 1;
     case {'claytonhac', 'gumbelhac', 'frankhac'}
         tree = hacfit(family(1:end-3), U);
         copulaparams.tree = tree;
+        
+        copulaparams.numParams = d - 1;
     otherwise
         error('Copula family %s not recognized.', family);
 end
