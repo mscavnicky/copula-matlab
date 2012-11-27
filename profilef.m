76b6d9bd288558ef7af7187ff0da3676f8a1db53
Martin Scavnicky <devnull@localhost>
2012-11-27 22:47:57 +0000

    implemented simple profiling function

---
+++ profilef.m
@@ -0,0 +1,17 @@
+function [ times ] = profilef( fun, n )
+%PROFILE Profile function
+
+times = zeros(n, 1);
+for i=1:n
+    tic();
+    fun();
+    times(i) = toc();
+    fprintf('Time: %f\n', times(i));
+end
+
+end
+
+% times = profilef(@()(ecopula(unifrnd(0, 1, 10000, 5))), 10);
+% times = profilef(@()(uniform(normrnd(0, 100, 30000, 5))), 20)
+
+
