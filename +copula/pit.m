e0cbb7d564aa56b017a2322e50c1f2b5c7194f1c
Martin Scavnicky <devnull@localhost>
2012-11-27 18:30:45 +0000

    fixed transformation bug where all columns but the first two were zeros

--- +copula/pit.m
+++ +copula/pit.m
@@ -3,7 +3,7 @@ function [ T ] = pit( family, U, copulaparams )
 %hypothesis that the data are generated using given copula.
 %
 %   References:
-%       [2] Breymann, Dependence Structures for Multivariate High-Frequency
+%       [1] Breymann, Dependence Structures for Multivariate High-Frequency
 %       Data in Finance, 2003
 
 [n, d] = size(U);
@@ -11,7 +11,7 @@ T = zeros(n, d);
 
 T(:,1) = U(:,1);
 for i=2:d
-    T(:,2) = copula.cnd( family, U, i, copulaparams );
+    T(:,i) = copula.cnd( family, U, i, copulaparams );
 end
 
 end
