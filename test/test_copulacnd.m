a1f2cbb15b2d8d9714d5113dbb6a3bc1700a34a7
Martin Scavnicky <devnull@localhost>
2012-11-27 23:55:45 +0000

    copula cnd unit test failure

--- test/test_copulacnd.m
+++ test/test_copulacnd.m
@@ -6,61 +6,66 @@ initTestSuite;
 
 function testGaussianIn2D
     U = csvread('data/data3d.csv');
-    rho = [1, 0.18, 0.23; 0.18, 1, 0.74; 0.23 0.74, 1];
-    X = copula.cnd('gaussian', U, 2, rho);
+    copulaparams.rho = covmat([0.18, 0.23, 0.74]);
+    X = copula.cnd('gaussian', U, 2, copulaparams);
     Y = csvread('data/test_copulacnd_gaussian2d.csv');
     assertVectorsAlmostEqual(X, Y);
     
 function testGaussianIn3D
     U = csvread('data/data3d.csv');
-    rho = [1, 0.18, 0.23; 0.18, 1, 0.74; 0.23 0.74, 1];
-    X = copula.cnd('gaussian', U, 3, rho);
+    copulaparams.rho = covmat([0.18, 0.23, 0.74]);
+    X = copula.cnd('gaussian', U, 3, copulaparams);
     Y = csvread('data/test_copulacnd_gaussian3d.csv');
     assertVectorsAlmostEqual(X, Y);
     
 function testStudentIn2D
     U = csvread('data/data3d.csv');
-    rho = [1, 0.37, 0.52; 0.37, 1, 0.77; 0.52, 0.77 1];
-    df = 5;
-    X = copula.cnd('t', U, 2, rho, df);
+    copulaparams.rho = covmat([0.37, 0.52, 0.77]);
+    copulaparams.nu = 5;    
+    X = copula.cnd('t', U, 2, copulaparams);
     Y = csvread('data/test_copulacnd_t2d.csv');
     assertVectorsAlmostEqual(X, Y, 'absolute', 0.05);
     
 function testStudentIn3D
     U = csvread('data/data3d.csv');
-    rho = [1, 0.37, 0.52; 0.37, 1, 0.77; 0.52, 0.77 1];
-    df = 5;
-    X = copula.cnd('t', U, 3, rho, df);
+    copulaparams.rho = covmat([0.37, 0.52, 0.77]);
+    copulaparams.nu = 5; 
+    X = copula.cnd('t', U, 3, copulaparams);
     Y = csvread('data/test_copulacnd_t3d.csv');
     assertVectorsAlmostEqual(X, Y, 'absolute', 0.07);
     
 function testClaytonIn2D
     U = csvread('data/data2d.csv');
-    X = copula.cnd('clayton', U, 2, 1.4557);
+    copulaparams.alpha = 1.4557;
+    X = copula.cnd('clayton', U, 2, copulaparams);
     Y = csvread('data/test_copulacnd_clayton2d.csv');
     assertVectorsAlmostEqual(X, Y);
     
 function testClaytonIn3D
     U = csvread('data/data3d.csv');
-    X = copula.cnd('clayton', U, 3, 0.9912);
+    copulaparams.alpha = 0.9912;
+    X = copula.cnd('clayton', U, 3, copulaparams);
     Y = csvread('data/test_copulacnd_clayton3d.csv');
     assertVectorsAlmostEqual(X, Y);
     
 function testGumbelIn3D
     U = csvread('data/data3d.csv');
-    X = copula.cnd('gumbel', U, 3, 1.4529);
+    copulaparams.alpha = 1.4529;
+    X = copula.cnd('gumbel', U, 3, copulaparams);
     Y = csvread('data/test_copulacnd_gumbel3d.csv');
     assertVectorsAlmostEqual(X, Y);
     
 function testFlatHacClaytonIn2D
     U = csvread('data/data2d.csv');
-    X = copula.cnd('clayton', U, 2, {1 2 1.4557});
+    copulaparams.tree = {1 2 1.4557};
+    X = copula.cnd('claytonhac', U, 2, copulaparams);
     Y = csvread('data/test_copulacnd_clayton2d.csv');
     assertVectorsAlmostEqual(X, Y);
     
 function testFlatHacClaytonIn3D
     U = csvread('data/data3d.csv');
-    X = copula.cnd('clayton', U, 3, {1 2 3 0.9912});
+    copulaparams.tree = {1 2 3 0.9912};
+    X = copula.cnd('claytonhac', U, 3, copulaparams);
     Y = csvread('data/test_copulacnd_clayton3d.csv');
     assertVectorsAlmostEqual(X, Y); 
     
