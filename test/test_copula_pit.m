97e0b3509208a7662f6c3a066be38a16eb997f98
Martin Scavnicky <devnull@localhost>
2012-11-28 00:06:34 +0000

    tested gaussian copula pit in 3d

--- test/test_copula_pit.m
+++ test/test_copula_pit.m
@@ -9,3 +9,9 @@ function testGaussianPitIn2D
     E = csvread('data/test_copula_pit_gaussian2d.csv');
     copulaparams.rho = covmat(0.57);
     assertVectorsAlmostEqual(E, copula.pit('gaussian', X, copulaparams));
+    
+ function testGaussianPitIn3D
+    X = csvread('data/data3d.csv');
+    E = csvread('data/test_copula_pit_gaussian3d.csv');
+    copulaparams.rho = covmat([0.18, 0.23, 0.74]);
+    assertVectorsAlmostEqual(E, copula.pit('gaussian', X, copulaparams));
--- test/test_data_generator.r
+++ test/test_data_generator.r
@@ -4,6 +4,7 @@ setwd("D:/Dropbox/Copulas/Matlab/test")
 data3 <- as.matrix(read.csv("data/data3d.csv", header=FALSE, sep=","))
 data2 <- as.matrix(read.csv("data/data2d.csv", header=FALSE, sep=","))
 
+normal.cop.2 = normalCopula(c(0.57), dim=2, dispstr="un")
 normal.cop.3 = normalCopula(c(0.18, 0.23, 0.74), dim=3, dispstr="un")
 t.cop.3 = tCopula(c(0.37, 0.52, 0.77), df=5, dim=3, dispstr="un")
 clayton.cop.3 = archmCopula("clayton", param=0.9912, dim=3)  
@@ -30,3 +31,7 @@ to.csv(testCond(data2, clayton.cop.2, 2), "data/test_copulacnd_clayton2d.csv")
 
 # Copula densities
 to.csv(dCopula(data3, clayton.cop.3), "data/test_archimpdf_clayton3d.csv")
+
+# Rosenblatt's transform
+to.csv(rtrafo(data2, normal.cop.2), "data/test_copula_pit_gaussian2d.csv")
+to.csv(rtrafo(data3, normal.cop.3), "data/test_copula_pit_gaussian3d.csv")
