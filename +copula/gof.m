e6c914e05b68adf61696d208a06d3c142c17b3d7
Martin Scavnicky <devnull@localhost>
2012-11-28 11:42:05 +0000

    refactored bootstrap procedure into single function

--- +copula/gof.m
+++ +copula/gof.m
@@ -1,49 +1,35 @@
 function [ h, p ] =  gof( family, U, N, method, copulaparams )
 %COPULAGOF Performs goodness-of-fit test given a fitted copula and data.
-%
-
-[n, d] = size(U);
 
 switch method
 case 'snc'
-    % Compute statistics value for original dataset
-    t = snc( family, U, copulaparams );
-    % Boostraped statistics
-    T = zeros(N, 1);
-    for i=1:N
-       %fprintf('GOF Boostrap %d...\n', i);
-       % Simulate fitted copula
-       V = uniform(copula.rnd(family, n, d, copulaparams));
-       % Fit simulated data
-       copulaparams = copula.fit(family, V);
-       % Get another bootstrapped statistics
-       T(i) = snc( family, V, copulaparams ); 
-    end
-    
-    p = mean(T > t);
-    h = p > 0.05;
+    [h, p] = bootstrap(@snc, N, family, U, copulaparams);
 case 'snb'
-    % Compute statistics value for original dataset
-    t = snc( family, U, copulaparams );
-    % Boostraped statistics
-    T = zeros(N, 1);
-    for i=1:N
-       fprintf('GOF Boostrap %d...\n', i);
-       % Simulate fitted copula
-       V = uniform(copula.rnd(family, n, d, copulaparams));
-       % Fit simulated data
-       copulaparams = copula.fit(family, V);
-       % Get another bootstrapped statistics
-       T(i) = snb( family, V, copulaparams ); 
-    end
-    
-    p = mean(T > t);
-    h = p > 0.05;
+    [h, p] = bootstrap(@snb, N, family, U, copulaparams);
 otherwise
     error('Method %s not recognized', method);
 end
 
+end
 
+function [h, p] = bootstrap( gof, N, family, U, copulaparams )
+    [n, d] = size(U);
+    % Compute statistics value for original dataset
+    t = gof( family, U, copulaparams );
+    % Boostraped statistics
+    T = zeros(N, 1);
+    for i=1:N       
+       % Simulate fitted copula
+       V = uniform(copula.rnd(family, n, d, copulaparams));
+       % Fit simulated data
+       copulaparams = copula.fit(family, V);
+       % Get another bootstrapped statistics
+       T(i) = gof( family, V, copulaparams ); 
+       dbg('%d: %f\n', i, T(i, 1));
+    end
+    
+    p = mean(T > t);
+    h = p > 0.05;
 end
 
 function [ T ] = snc( family, U, copulaparams )
