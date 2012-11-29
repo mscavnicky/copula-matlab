8a07d4c022c060652b03b3f31f7bf4055bab882b
Martin Scavnicky <devnull@localhost>
2012-11-29 18:43:27 +0000

    refactored random sampling from archimedean copulas

--- +archim/rnd.m
+++ +archim/rnd.m
@@ -4,19 +4,32 @@ function [ U ] = rnd( family, alpha, n, d )
 %   References:
 %       McNeil
 
+% Special method
+%X = unifrnd(0, 1, n, d);
+%copulaparams.alpha = alpha;
+%U = copula.pit( family, X, copulaparams );
+%return;
+
 switch family
 case 'clayton' 
     V = gamrnd(1/alpha, 1, n, 1);
     X = unifrnd(0, 1, n, d);
-    U = archim.gen(family, bsxfun(@rdivide, -log(X), V), alpha);
+    U = archim.gen(family, -log(X) ./ repmat(V, 1, d), alpha);
 case 'gumbel'
     V = stablernd(1/alpha, 1, n) * (cos(pi/(2*alpha)))^alpha;
+    %V = stblrnd(1/alpha, 1, cos(pi/(2*alpha))^alpha, 0, n, 1);
     X = unifrnd(0, 1, n, d);
-    U = archim.gen(family, bsxfun(@rdivide, -log(X), V), alpha);
+    U = archim.gen(family, -log(X) ./ repmat(V, 1, d), alpha);
+    %V = rand(n,1)*pi-pi/2;
+    %W = -log(rand(n,1));
+    %T = V + pi/2;
+    %gamma = sin(T/alpha).^(1/alpha).*cos(V).^(-1).*((cos(V-T/alpha))./W).^(1-1/alpha);
+    %U = exp( - (-log(rand(n,d))).^(1/alpha)./(gamma*ones(1,d))  );
 case 'frank'
-    % Taken from Polish guys thesis
-    gamma = floor(1+log(rand(n,1)) ./ (log(1-exp(-alpha*rand(n,1)))));
-    U = -log(1-exp(log(rand(n,d))./(gamma*ones(1,d)))*(1-exp(-alpha)))/alpha;
+    % Log-series sampling taken from polish guys thesis
+    V = floor(1+log(rand(n,1)) ./ log(1-exp(-alpha*rand(n,1))));
+    X = unifrnd(0, 1, n,d);   
+    U = archim.gen(family, -log(X) ./ repmat(V, 1, d), alpha);
 otherwise
     error('Copula family %s not recognized.', family);    
 end
