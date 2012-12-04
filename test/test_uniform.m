6d9ac7d7cfcf72da4e60bc1ed3e85683f085f381
Martin Scavnicky <devnull@localhost>
2012-12-04 15:49:14 +0000

    uniform now works only with column vectors

--- test/test_uniform.m
+++ test/test_uniform.m
@@ -7,15 +7,10 @@
 function test_suite = test_uniform
 initTestSuite;
 
-function testUniformRowVector
-    x = [1 2 3 3 4 5 1];
-    expected = [0.25 0.375 0.625 0.625 0.75 0.875 0.25];
-    assertVectorsAlmostEqual(expected, uniform(x, 2));
-    
 function testUniformColumnVector
     x = [1; 2; 3; 3; 4; 5; 5];
     expected = [0.125; 0.25; 0.5; 0.5; 0.625; 0.875; 0.875];
-    assertVectorsAlmostEqual(expected, uniform(x, 1));
+    assertVectorsAlmostEqual(expected, uniform(x));
     
 function testUniformMatrix
     X = [1 2 3; 4 5 6];
--- uniform.m
+++ uniform.m
@@ -1,5 +1,5 @@
-function [ U ] = uniform( X, dim )
-%UNIFORM Converts matrix to uniform variates in a specified dimension.
+function [ U ] = uniform( X )
+%UNIFORM Converts matrix to uniform variates by columns.
 %   Based on empirical CDF function described in [1]. We use n+1 for
 %   division to keep empirical CDF lower than 1. 
 %
@@ -11,29 +11,14 @@ function [ U ] = uniform( X, dim )
 %       [1] Berg, D. Bakken, H. (2006) Copula Goodness-of-fit Tests: A
 %       Comparative Study
 
-if nargin < 2
-    dim = 1;
-end
-
 [m, n] = size(X);
 U = zeros(m, n);
 
-if dim == 1 % uniform cols
-    for i=1:n % for each column
-        S = sort(X(:,i));
-        for j=1:m
-            U(j, i) = ismembc2(X(j,i), S) / (m + 1);
-        end
-    end     
-elseif dim == 2 % uniform rows
-    for i=1:m % for each row
-        S = sort(X(i,:));
-        for j=1:n
-            U(i, j) = ismembc2(X(i,j), S) / (n + 1);
-        end
-    end    
-else
-    error 'Incorrect dimension.'
+for i=1:n
+    S = sort(X(:,i));
+    for j=1:m
+        U(j, i) = ismembc2(X(j,i), S) / (m + 1);
+    end
 end
 
 end
