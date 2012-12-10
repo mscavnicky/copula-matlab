57c2e9b7194ff58e91fbf5e8428c08cc3047da27
Martin Scavnicky <devnull@localhost>
2012-12-10 00:13:46 +0000

    drawing of gaussian and student copulas

--- copulaimg.m
+++ copulaimg.m
@@ -1,15 +1,24 @@
-function copulaimg( fun, fname )
+function copulaimg( fun, fname, clines, min, max )
+
+if (nargin < 3)
+    clines = 10;
+end
+
+if (nargin < 5)
+    min = 0;
+    max = 1;
+end
 
 fig = figure(1);
 set(fig, 'Position', [0, 0, 800, 400]);
 
 sub1 = subplot(1,2,1);
-[U1, U2, Y] = ugrid(fun, 20);
+[U1, U2, Y] = ugrid(fun, 20, min, max);
 surf(U1, U2, Y);
 
 sub2 = subplot(1,2,2);
-[U1, U2, Y] = ugrid(fun, 100);
-contour(U1, U2, Y);
+[U1, U2, Y] = ugrid(fun, 100, min, max);
+contour(U1, U2, Y, clines);
 
 set(sub1,'Units','normalized', 'position', [0.05 0.12 0.4 0.8]);
 set(sub2,'Units','normalized', 'position', [0.58 0.17 0.35 0.7]);
@@ -21,9 +30,9 @@ print('-dpng', '-r300', fname);
 
 end
 
-function [ U1, U2, C ] = ugrid( fun, n )
-%UNIFGRID Returns grid useful for graphical display.
-u = linspace(0, 1, n);
+function [ U1, U2, C ] = ugrid( fun, n, min, max )
+%UGRID Returns grid useful for graphical display.
+u = linspace(min, max, n);
 [U1, U2] = meshgrid(u, u);
 U = [U1(:) U2(:)];
 C = reshape(fun(U), n, n);
--- thesis.m
+++ thesis.m
@@ -6,3 +6,9 @@ copulaimg(@(U) min(U, [], 2), '../Images/comonotonicity-cdf.png');
 
 %% Countermonotonicity copula
 copulaimg(@(U) max(sum(U, 2) - 1, 0), '../Images/countermonotonicity-cdf.png');
+
+%% Guassian copula
+copulaimg(@(U) copulapdf('gaussian', U, 0.3), '../Images/gaussian-pdf.png', 20, 0.05, 0.95);
+
+%% Student-t copula
+copulaimg(@(U) copulapdf('t', U, 0.3, 3), '../Images/student-pdf.png', 20, 0.05, 0.95);
