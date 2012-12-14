62567a07a79b4e89fbdd9c6972dd664b6d3331d7
Martin Scavnicky <devnull@localhost>
2012-12-14 00:40:45 +0000

    moved laplace-stieltes transform sampling into separate function

--- +archim/rnd.m
+++ +archim/rnd.m
@@ -12,24 +12,8 @@ function [ U ] = rnd( family, alpha, n, d )
 %U = copula.pit( family, X, copulaparams );
 %return;
 
-switch family
-case 'clayton' 
-    V = gamrnd(1/alpha, 1, n, 1);
-    X = unifrnd(0, 1, n, d);
-    U = archim.gen(family, -log(X) ./ repmat(V, 1, d), alpha);
-case 'gumbel'
-    % Testing of generating Gumbel copula number in R is done differently
-    % then we are doing it. They use gamma=1. Our implementation was
-    % succesfully tested against MATLAB implementation.
-    V = stblrnd(1/alpha, 1, cos(pi/(2*alpha))^alpha, 0, n, 1);
-    X = unifrnd(0, 1, n, d);
-    U = archim.gen(family, -log(X) ./ repmat(V, 1, d), alpha);
-case 'frank'
-    V = logrnd(1 - exp(-alpha), n);
-    X = unifrnd(0, 1, n, d);   
-    U = archim.gen(family, -log(X) ./ repmat(V, 1, d), alpha);
-otherwise
-    error('Copula family %s not recognized.', family);    
-end
+V = archim.lsinvrnd(family, alpha, n);
+X = rand(n, d);
+U = archim.gen(family, -log(X) ./ repmat(V, 1, d), alpha);
 
 end
