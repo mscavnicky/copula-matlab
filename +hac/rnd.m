342b5a46e98085d250fcaf9974c5c6e1b7a70fbe
Martin Scavnicky <devnull@localhost>
2012-12-14 22:38:22 +0000

    finished implementation hierarchical archimedean copulas sampling

--- +hac/rnd.m
+++ +hac/rnd.m
@@ -31,6 +31,19 @@ alpha = theta0 / theta1;
 
 switch family
 case 'clayton'
+    M = findOptimumM(V0);
+    gamma = (cos(alpha * pi / 2) .* V0 ./ M)^(1/alpha);
+    
+    V01 = zeros(n, 1);    
+    for i=1:n
+       while true
+          v = stblrnd(alpha, 1, gamma(i), 0);
+          if (rand() <= exp(-v))
+              V01(i) = v;
+              break;
+          end
+       end
+    end
     
 case 'gumbel'
     gamma = (cos(alpha * pi / 2) .* V0)^(1/alpha);
@@ -40,11 +53,11 @@ case 'frank'
     % Log-Series distribution parameter
     c1 = 1 - exp(-theta1);
     % Preallocation of return vector
-    V01 = zeros(n, 1);    
+    V01 = zeros(n, 1);
     
     for i=1:n    
         if abs(theta0) < 1
-            while 1
+            while true
                 U = rand();
                 X = logrnd(c1);
                 if U < 1/((X-alpha)*beta(X, 1-alpha))
@@ -53,7 +66,7 @@ case 'frank'
                 end
             end
         else
-            while 1
+            while true
                 U = rand();
                 X = sibuyarnd(alpha, 1);
                 if U <= c1^(X-1)
@@ -64,6 +77,20 @@ case 'frank'
         end
     end
 end
+
+end
+
+function [ M ] = findOptimumM( V0 )
+
+floorV = floor(V0);
+ceilV = ceil(V0);
+v1 = floorV * exp(V0 / floorV);
+v2 = ceilV * exp(V0 / ceilV);
+
+M = zeros(length(V0));
+M(V0 <= 1) = 1;
+M(V0 > 1 & v1 <= v2) = floorV(V0 > 1 & v1 <= v2);
+M(V0 > 1 & v1 > v2) = ceilV(V0 > 1 & v1 > v2);
 
 end
 
