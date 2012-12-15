9c6a8aa6338bc0c50e120335379edb0382052270
Martin Scavnicky <devnull@localhost>
2012-12-15 11:04:40 +0000

    fixed bug in sibuyarnd

--- sibuyarnd.m
+++ sibuyarnd.m
@@ -6,12 +6,14 @@ function [ X ] = sibuyarnd( alpha, n )
 %       2011
 
 U = rand(n, 1);
-G = ((1 - U) * gamma(1 - alpha)).^(-1/alpha);
-F = 1 - (1 / (G * beta(G, 1- alpha)));
+G = ((1 - U) * gamma(1 - alpha)) .^ (-1/alpha);
+fG = floor(G);
+cG = ceil(G);
+F = 1 - (1 ./ (fG .* beta(fG, 1 - alpha)));
 
 X = zeros(n, 1);
-X(F < U) = ceil(G(F < U));
-X(F >= U) = floor(G(F >= U));
+X(F < U) = cG(F < U);
+X(F >= U) = fG(F >= U);
 X(U <= alpha) = 1;
 
 end
