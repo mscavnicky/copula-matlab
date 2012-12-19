63d6294ec66c7311bb2b06d47364cc55c1bea876
Martin Scavnicky <devnull@localhost>
2012-12-19 22:20:33 +0000

    added new plots for thesis

--- thesis.m
+++ thesis.m
@@ -12,3 +12,12 @@ copulaimg(@(U) copulapdf('gaussian', U, 0.3), '../Images/gaussian-pdf.png', 20, 
 
 %% Student-t copula
 copulaimg(@(U) copulapdf('t', U, 0.3, 3), '../Images/student-pdf.png', 20, 0.05, 0.95);
+
+%% Clayton copula
+copulaimg(@(U) copulapdf('clayton', U, 0.7), '../Images/clayton-pdf.png', 30, 0.05, 0.95);
+
+%% Gumbel copula
+copulaimg(@(U) copulapdf('gumbel', U, 1.3), '../Images/gumbel-pdf.png', 20, 0.05, 0.95);
+
+%% Frank copula
+copulaimg(@(U) copulapdf('frank', U, -2), '../Images/frank-pdf.png', 20, 0.05, 0.95);
