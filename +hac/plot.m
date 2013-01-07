77cd01641b6496a4f2043a949bdbaabd58b73ccc
Martin Scavnicky <devnull@localhost>
2013-01-07 18:36:57 +0000

    hac plot now prints labels

--- +hac/plot.m
+++ +hac/plot.m
@@ -4,28 +4,39 @@ function plot( tree )
 %   References:
 %       [1] http://stackoverflow.com/questions/5065051/add-node-numbers-get-node-locations-from-matlabs-treeplot
 
-[nodes, count] = hac2nodes( tree, [], 1 );
+% Parent of the root node is 0
+nodes(1) = 0;
+% Label of the root node is its alpha
+labels{1} = tree{end};
+% Run DFS to obtain nodes and labels
+[nodes, labels, ~] = hac2nodes( tree, nodes, labels, 1 );
+% Plot the tree using nodes
 treeplot(nodes);
+% Write the labels
 [x, y] = treelayout(nodes);
-name1 = cellstr(num2str((1:count)'));
-text(x, y, name1, 'VerticalAlignment','bottom','HorizontalAlignment','right')
+text(x, y, labels, 'VerticalAlignment','bottom','HorizontalAlignment','right')
 
 end
 
-function [ nodes, count ] = hac2nodes( tree, nodes, count )
+function [ nodes, labels, count ] = hac2nodes( tree, nodes, labels, count )
 %HAC2NODES Given the tree and number of root copula returns nodes for
 %treeplot and number of last numbered copula and names.
 
-parent = count;
-fprintf('%s: %d\n', dprint(tree), count);
+% fprintf('%s: %d\n', dprint(tree), count);
 
+parent = count;
 for i=1:length(tree)-1
-    count = count+1;
-    nodes(count) = parent;
-    if iscell(tree{i})     
-        [nodes, count] = hac2nodes(tree{i}, nodes, count);
+    count = count + 1;
+    nodes(count) = parent; 
+    child = tree{i};
+    if iscell(child)        
+        % Use alpha as a label of this subtree
+        labels{count} = child{end};
+        [nodes, labels, count] = hac2nodes(child, nodes, labels, count);        
     else
-        fprintf('%d: %d\n', tree{i}, count);
+        % Use variable number as a label of this subtree
+        labels{count} = child;
+        % fprintf('%d: %d\n', child, count);        
     end
 end
 
