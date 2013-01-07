14f64720b2b8d1e4836d901a10c505d009097f74
Martin Scavnicky <devnull@localhost>
2013-01-07 17:51:35 +0000

    more work on chemistry and wine datasets

--- chemistry.m
+++ chemistry.m
@@ -31,6 +31,12 @@ U = uniform(data);
 UX = uniform(X);
 UY = uniform(Y);
 
+UY1 = UY(:, 1);
+UY2 = UY(:, 2);
+
+UX_upper_by_Y1 = UX(UY1 > median(UY1), :);
+UX_lower_by_Y1 = UX(UY1 <= median(UY1), :);
+
 %% Uniformed histograms
 
 %Histograms for U3 and U4 is deformed
@@ -39,7 +45,44 @@ hist(U(:,2));
 hist(U(:,3));
 hist(U(:,4));
 
-%% Fit copulas
+%% Fit input variables using copulas
+
+f1 = copula.eval('gaussian', UX, 1000);
+f2 = copula.eval('t', UX, 10);
+f3 = copula.eval('clayton', UX, 1000);
+f4 = copula.eval('gumbel', UX, 1000);
+f5 = copula.eval('frank', UX, 1000);
+
+matrix2latex([f1;f2;f3;f4;f5], 'fit.tex',...
+'alignment', 'r', 'format', '%.3f',...
+'rowLabels', {'Gaussian', 't', 'Clayton', 'Gumbel', 'Frank'},...
+'columnLabels', {'LL', 'AIC', 'BIC', 'SnC', 'SnB'});
+
+%% Fit input variables using treshold for Y
+
+f1 = copula.eval('gaussian', UX_lower_by_Y1, 1000);
+f2 = copula.eval('t', UX_lower_by_Y1, 10);
+f3 = copula.eval('clayton', UX_lower_by_Y1, 1000);
+f4 = copula.eval('gumbel', UX_lower_by_Y1, 1000);
+f5 = copula.eval('frank', UX_lower_by_Y1, 1000);
+
+matrix2latex([f1;f2;f3;f4;f5], 'fit.tex',...
+'alignment', 'r', 'format', '%.3f',...
+'rowLabels', {'Gaussian', 't', 'Clayton', 'Gumbel', 'Frank'},...
+'columnLabels', {'LL', 'AIC', 'BIC', 'SnC', 'SnB'});
+
+f1 = copula.eval('gaussian', UX_upper_by_Y1, 1000);
+f2 = copula.eval('t', UX_upper_by_Y1, 10);
+f3 = copula.eval('clayton', UX_upper_by_Y1, 1000);
+f4 = copula.eval('gumbel', UX_upper_by_Y1, 1000);
+f5 = copula.eval('frank', UX_upper_by_Y1, 1000);
+
+matrix2latex([f1;f2;f3;f4;f5], 'fit.tex',...
+'alignment', 'r', 'format', '%.3f',...
+'rowLabels', {'Gaussian', 't', 'Clayton', 'Gumbel', 'Frank'},...
+'columnLabels', {'LL', 'AIC', 'BIC', 'SnC', 'SnB'});
+
+%% Fit input variables using tresholding
 
 % Data are not suitable for copulas
 % GOF's are zero
@@ -61,8 +104,4 @@ copula.eval('clayton', UY, 1000);
 copula.eval('gumbel', UY, 1000);
 copula.eval('frank', UY, 1000);
 
-copula.eval('gaussian', UX, 1000);
-copula.eval('t', UX, 10);
-copula.eval('clayton', UX, 1000);
-copula.eval('gumbel', UX, 1000);
-copula.eval('frank', UX, 1000);
+
--- wine.m
+++ wine.m
@@ -7,6 +7,55 @@ Y = data(:,12);
 
 U = uniform(X);
 
+n = size(X, 1);
+
+%% Asses fit of the margins
+
+% fixed acidity
+[D1, PD1] = allfitdist(X(:,1)); % NO (Inverse Gaussian)
+[h, p] = kstest2(X(:,1), PD1{2}.random(n, 1))
+
+% volatile acidity
+[D2, PD2] = allfitdist(X(:,2)); % Gamma distribution
+[h, p] = kstest2(X(:,2), PD2{1}.random(n, 1))
+
+% citric acid
+[D3, PD3] = allfitdist(X(:,3)); % NO (Exponential)
+[h, p] = kstest2(X(:,3), PD3{2}.random(n, 1))
+
+% residual sugar
+[D4, PD4] = allfitdist(X(:,4)); % NO (loglogistic)
+[h, p] = kstest2(X(:,4), PD4{3}.random(n, 1))
+
+% chlorides
+[D5, PD5] = allfitdist(X(:,5)); % t
+[h, p] = kstest2(X(:,5), PD5{1}.random(n, 1))
+
+% free sulfur dioxide
+[D6, PD6] = allfitdist(X(:,6)); % NO (Gamma)
+[h, p] = kstest2(X(:,6), PD6{2}.random(n, 1))
+
+% total sulfur dioxide
+[D7, PD7] = allfitdist(X(:,7)); % Lognormal
+[h, p] = kstest2(X(:,7), PD7{3}.random(n, 1))
+
+% density
+[D8, PD8] = allfitdist(X(:,8)); % t
+[h, p] = kstest2(X(:,8), PD8{3}.random(n, 1))
+
+% pH
+[D9, PD9] = allfitdist(X(:,9)); % t
+[h, p] = kstest2(X(:,9), PD9{3}.random(n, 1))
+
+% sulphates
+[D10, PD10] = allfitdist(X(:,10)); % Generalized Extreme Value
+[h, p] = kstest2(X(:,10), PD10{1}.random(n, 1))
+
+% alcohol
+[D11, PD11] = allfitdist(X(:,11)); % NO Generalized Extreme Value
+[h, p] = kstest2(X(:,11), PD11{1}.random(n, 1))
+
+
 %% Histograms
 
 hist(data(:,1), 20);
@@ -22,12 +71,38 @@ hist(data(:,10), 20);
 hist(data(:,11), 20);
 hist(data(:,12), 20);
 
-%% Fit copulas using CFM
+%% Fit copulas using CFM in 11 dimensions
 
-copula.eval('gaussian', U, 100);
-copula.eval('t', U, 10);
-copula.eval('clayton', U, 100);
-copula.eval('gumbel', U, 10);
-copula.eval('frank', U, 100);
+f1 = copula.eval('gaussian', U, 100);
+%f2 = copula.eval('t', U, 10);
+f3 = copula.eval('clayton', U, 100);
+f4 = copula.eval('gumbel', U, 10);
+f5 = copula.eval('frank', U, 100);
+
+matrix2latex([f1;f3;f4;f5], 'fit.tex',...
+'alignment', 'r', 'format', '%.3f',...
+'rowLabels', {'Gaussian', 'Clayton', 'Gumbel', 'Frank'},...
+'columnLabels', {'LL', 'AIC', 'BIC', 'SnC', 'SnB'});
+
+%% Fit copulas using CFM in 4 dimensions
+S = U(Y==7, [8,9]);
+
+f1 = copula.eval('gaussian', S, 200);
+f2 = copula.eval('t', S, 11);
+f3 = copula.eval('clayton', S, 200);
+f4 = copula.eval('gumbel', S, 200);
+f5 = copula.eval('frank', S, 200);
+f6 = copula.eval('claytonhac', S, 0, 'okhrin');
+f7 = copula.eval('gumbelhac', S, 0, 'okhrin');
+f8 = copula.eval('frankhac', S, 0, 'okhrin');
+
+matrix2latex([f1;f2;f3;f4;f5;[f6, NaN, NaN];[f7 NaN NaN];[f8 NaN NaN]], 'fit.tex',...
+'alignment', 'r', 'format', '%.3f',...
+'rowLabels', {'Gaussian', 't', 'Clayton', 'Gumbel', 'Frank', 'Clayton HAC', 'Gumbel HAC', 'Frank HAC'},...
+'columnLabels', {'LL', 'AIC', 'BIC', 'SnC', 'SnB'});
+
+
+
+
 copula.eval('claytonhac', U, 0, 'okhrin');
 copula.eval('gumbelhac', U, 10, 'okhrin');
