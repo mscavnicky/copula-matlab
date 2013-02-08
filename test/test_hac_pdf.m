9caeb4a97154e8ae1f3849ed5f310f168a2b00fb
Martin Scavnicky <devnull@localhost>
2013-02-08 23:40:56 +0000

    tested flat hac pdf in 3 dimensions

--- test/test_hac_pdf.m
+++ test/test_hac_pdf.m
@@ -5,16 +5,30 @@
 function test_suite = test_hac_pdf
 initTestSuite;
 
-function testHacPdf
+function testHacPdf2D
 % Tests 2 dimensional case of hac.fpdf with copulapdf
     U = csvread('data/data2d.csv');
-    assertVectorsAlmostEqual(hac.pdf('clayton', U, { 1 2 [1.5] }), copulapdf('clayton', U, 1.5));
-    assertVectorsAlmostEqual(hac.pdf('frank', U,  { 1 2 [1.5] }), copulapdf('frank', U, 1.5));
-    assertVectorsAlmostEqual(hac.pdf('gumbel', U, { 1 2 [1.5] }), copulapdf('gumbel', U, 1.5));
+    assertVectorsAlmostEqual(hac.pdf('clayton', U, { 1 2 1.5 }), copulapdf('clayton', U, 1.5));
+    assertVectorsAlmostEqual(hac.pdf('frank', U,  { 1 2 1.5 }), copulapdf('frank', U, 1.5));
+    assertVectorsAlmostEqual(hac.pdf('gumbel', U, { 1 2 1.5 }), copulapdf('gumbel', U, 1.5));
     
-function testHacFastPdf
+function testHacPdf3D
+% Tests 2 dimensional case of hac.fpdf with copulapdf
+    U = csvread('data/data3d.csv');
+    assertVectorsAlmostEqual(hac.pdf('clayton', U, { 1 2 3 1.5 }), archim.pdf('clayton', U, 1.5));
+    assertVectorsAlmostEqual(hac.pdf('frank', U,  { 1 2 3 1.5 }), archim.pdf('frank', U, 1.5));
+    assertVectorsAlmostEqual(hac.pdf('gumbel', U, { 1 2 3 1.5 }), archim.pdf('gumbel', U, 1.5));
+    
+function testHacFastPdf2D
 % Tests 2 dimensional case of hac.fpdf with copulapdf
     U = csvread('data/data2d.csv');
-    assertVectorsAlmostEqual(hac.fpdf('clayton', U, { 1 2 [1.5] }), copulapdf('clayton', U, 1.5));
-    assertVectorsAlmostEqual(hac.fpdf('frank', U,  { 1 2 [1.5] }), copulapdf('frank', U, 1.5));
-    assertVectorsAlmostEqual(hac.fpdf('gumbel', U, { 1 2 [1.5] }), copulapdf('gumbel', U, 1.5));
+    assertVectorsAlmostEqual(hac.fpdf('clayton', U, { 1 2 1.5 }), copulapdf('clayton', U, 1.5));
+    assertVectorsAlmostEqual(hac.fpdf('frank', U,  { 1 2 1.5 }), copulapdf('frank', U, 1.5));
+    assertVectorsAlmostEqual(hac.fpdf('gumbel', U, { 1 2 1.5 }), copulapdf('gumbel', U, 1.5));
+    
+function testHacFastPdf3D
+% Tests 2 dimensional case of hac.fpdf with copulapdf
+    U = csvread('data/data3d.csv');
+    assertVectorsAlmostEqual(hac.fpdf('clayton', U, { 1 2 3 1.5 }), archim.pdf('clayton', U, 1.5));
+    assertVectorsAlmostEqual(hac.fpdf('frank', U,  { 1 2 3 1.5 }), archim.pdf('frank', U, 1.5));
+    assertVectorsAlmostEqual(hac.fpdf('gumbel', U, { 1 2 3 1.5 }), archim.pdf('gumbel', U, 1.5));
