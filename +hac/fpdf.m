46a1e6d0fd1add999830a1d7ff32120229a86032
Martin Scavnicky <devnull@localhost>
2013-02-09 14:06:08 +0000

    refactored hac fpdf to enable further changes

--- +hac/fpdf.m
+++ +hac/fpdf.m
@@ -17,46 +17,9 @@ end
 
 % Replace terms inside it with variables
 [inexpr, terms] = hac.fpdf.substitute(char(fexpr));
-
 % Convert infix expression into its postfix form
 postexpr = hac.fpdf.in2post(inexpr);
-
-% Initialized empty cdf cache
-cdfcache = containers.Map;
-% Evaluate the postfix form
-stack = {};
-top = 0;
-
-for i=1:numel(postexpr)
-    token = postexpr{i};
-    
-    if regexp(token, '\+|\*|\^') == 1
-        T2 = stack{top}; top = top - 1;
-        T1 = stack{top}; top = top - 1;
-        
-        if strcmp(token, '+')
-            T = T1 + T2;
-        elseif strcmp(token, '*')
-            T = T1 .* T2;
-        elseif strcmp(token, '^')
-            T = T1 .^ T2;
-        end
-        
-        top = top + 1; stack{top} = T;
-    elseif regexp(token, 't_[0-9]+_[0-9]+') == 1
-        term = terms(token);        
-        [T, cdfcache] = hac.fpdf.evalterm(family, term, U, params, cdfcache);
-        top = top + 1; stack{top} = T;
-    else
-        [T, cdfcache] = hac.fpdf.evalterm(family, token, U, params, cdfcache);
-        top = top + 1; stack{top} = T;
-    end    
-end
-
-if top ~= 1
-    error('Implementation error in hacfpdf.');
-end
-
-Y = stack{1};
+% Evaluate substituted expression in a postfix form
+Y = hac.fpdf.evalpostfix(family, U, postexpr, terms, params);
 
 end
