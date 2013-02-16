a22f16141bd10c014ba77613a7694cebea1b4d89
Martin Scavnicky <devnull@localhost>
2013-02-16 17:20:36 +0000

    hac fpdf now does not print debugging expressions

--- +hac/+fpdf/diffexpr.m
+++ +hac/+fpdf/diffexpr.m
@@ -2,10 +2,10 @@ function [ dexpr ] = diffexpr( expr )
 %DIFFEXPR Differentiate expression given in a string
 
 fexpr = sym(expr);
-vars = symvar(fexpr)
+vars = symvar(fexpr);
 for i=1:numel(vars)
     fexpr = diff(fexpr, vars(i));
-    char(fexpr)
+    %char(fexpr)
 end
 
 dexpr = char(fexpr);
