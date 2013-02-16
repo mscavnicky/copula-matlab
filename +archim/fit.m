4fcfe9856f8bb3ef05fa9ce8b7ff31c88f17358a
Martin Scavnicky <devnull@localhost>
2013-02-16 09:47:18 +0000

    improved upper bound correction in archim fit

--- +archim/fit.m
+++ +archim/fit.m
@@ -2,11 +2,10 @@ function [ alphahat ] = fit( family, U, lowerBound, upperBound )
 %ARCHIMFIT Fit multivariate archimedean copula to data.
 %   Data must be within interval [0, 1]. Returns value of fitted parameters
 %   and likelihood of fit to data. Minimization is performed using fminbnd
-%   method which requires methods to be continuouos. This assumption should
-%   be checked in Nelsen.
+%   method which requires methods to be continuouos.
 
 % Function giving likelihood of the sample for given alpha
-fun = @(alpha) loglike(archim.pdf( family, U, alpha ));
+optimfun = @(alpha) loglike(archim.pdf( family, U, alpha ));
 
 % Get bound for Archimedean copula family in this dimension
 if nargin < 3
@@ -15,21 +14,26 @@ end
 
 % Debugging function plot
 if 0
-    L = linspace(max(lowerBound, -10), min(upperBound, 10), 100);
+    L = linspace(0.1, min(upperBound, 20), 100);
     fig = figure;
     plot(L, arrayfun( @(alpha)(loglike(archim.pdf( family, U, alpha ))), L));
     pause(10);
     close(fig);
 end
 
-% Perform the actual minimization using clipped bounds
-alphahat = estimateAlpha(fun, max(lowerBound, -10), min(upperBound, 10));
-if abs(alphahat) > 9.9
-    %warning('Alpha limit too small %f.', alphahat);
-    alphahat = estimateAlpha(fun, max(lowerBound, -100), min(upperBound, 100));
-    if abs(alphahat) > 99.9
-        warning('Alpha not estimated correctly %f.', alphahat);
+% Find some reasonable upper bound
+if upperBound == Inf
+    newUpperBound = 4;    
+    while optimfun(newUpperBound) > optimfun(newUpperBound * 2)
+        newUpperBound = newUpperBound * 2;
     end
+    upperBound = newUpperBound * 2;
+end
+
+alphahat = estimateAlpha(optimfun, max(lowerBound, -10), upperBound);
+
+if abs(alphahat - upperBound) < 0.01
+    warning('archim:fit:alpha', 'Estimated alpha too close to upper bound.');
 end
 
 end
