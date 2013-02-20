469a192f1c76ebf6b606512f408860cea0883dd9
Martin Scavnicky <devnull@localhost>
2013-02-20 18:47:13 +0000

    fpdf diffexpr now takes number of variables as an argument

--- +hac/+fpdf/diffexpr.m
+++ +hac/+fpdf/diffexpr.m
@@ -1,14 +1,26 @@
-function [ dexpr ] = diffexpr( expr )
-%DIFFEXPR Differentiate expression given in a string
+function [ diffExpr ] = diffexpr( expr, m )
+%HAC.FPDF.DIFFEXPR Differentiate copula expression string.
+%   Given a copula expression string differentiates with respect to the
+%   first m arguments using Symbolic Toolbox and returns string of the
+%   differentiated expression.
+%
+%   When second argument is not provided, expression is differentiated with
+%   respect to all variables.
 
-fexpr = sym(expr);
-vars = symvar(fexpr);
-for i=1:numel(vars)
-    fexpr = diff(fexpr, vars(i));
-    %char(fexpr)
+% Convert copula string expression into symbolic form
+symExpr = sym(expr);
+% Obtain all the variables of the symbolic expression
+vars = symvar(symExpr);
+% Expression is differentiated wrt all arguments
+if nargin < 2
+    m = numel(vars);
 end
-
-dexpr = char(fexpr);
+% Differentiate in the first m arguments
+for i=1:m
+    symExpr = diff(symExpr, vars(i));
+end
+% Convert differentiated symbolic expression back to string
+diffExpr = char(symExpr);
 
 end
 
