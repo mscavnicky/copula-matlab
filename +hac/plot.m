148d8ec9fdffd3c78dce60ccfba9bb400ce9d80e
Martin Scavnicky <devnull@localhost>
2013-02-20 14:39:55 +0000

    improved visuals of hac plot

--- +hac/plot.m
+++ +hac/plot.m
@@ -1,4 +1,4 @@
-function plot( family, tree, names )
+function plot( family, tree, names, imagename )
 %HAC.PLOT Plot structure of given HAC tree.
 %   Generates a figure of HAC where each leaf represents an attribute and
 %   each inner node represents an Archimedean copula. Parameter of the
@@ -10,27 +10,58 @@ function plot( family, tree, names )
 % Parent of the root node is 0
 nodes(1) = 0;
 % Label of the root node is its alpha
-labels{1} = sprintf('\\tau=%.3f', copulastat(family, tree{end}));
+labels{1} = sprintf('$%.3f$', copulastat(family, tree{end}));
 % Run DFS to obtain nodes and labels
 [nodes, labels, count] = hac2nodes( family, tree, nodes, labels, 1 );
 % Plot the tree using nodes
 treeplot(nodes);
+% Turn off all the borders in the picture
+set(gca, 'Box','off')
+set(gca, 'XTick', [], 'YTick', []);
+% Remevoes the height label
+set(get(gca, 'XLabel'), 'String', []);
+set(gca, 'Visible', 'off');
+
 % Write the labels
 [x, y] = treelayout(nodes);
 % Use column names if they are available
-if nargin > 1
+if nargin > 2
    for i=1:count
       if ~ischar(labels{i})
           u = labels{i};
           if u > 0
             labels{i} = sprintf('%s', names{u});
           else
-            labels{i} = sprintf('*%s', names{abs(u)});
+            labels{i} = sprintf('%s*', names{abs(u)});
           end
       end
    end
 end
-text(x, y, labels, 'VerticalAlignment','bottom','HorizontalAlignment','right')
+
+% Plot node labels
+for i=1:numel(labels)
+    if regexp(labels{i}, '[01]\.[0-9]+')
+        th = text(x(i)+0.02, y(i), labels{i});
+        set(th, 'Interpreter', 'latex');    
+        set(th, 'VerticalAlignment', 'bottom');
+        set(th, 'HorizontalAlignment', 'left');        
+    else
+        th = text(x(i), y(i), labels{i});
+        set(th, 'Interpreter', 'latex');    
+        set(th, 'VerticalAlignment', 'bottom');
+        set(th, 'HorizontalAlignment', 'right');
+        set(th, 'Rotation', 55);       
+    end
+end
+
+% Print to file if argument specified
+if nargin > 3
+    set(gcf, 'PaperUnits', 'centimeters');
+    set(gcf, 'PaperSize', [5.0 6.0]);
+    set(gcf, 'PaperPosition', [0 0 5.0 6.0]);
+    print('-dpdf', '-r300', imagename);
+end
+
 
 end
 
@@ -50,7 +81,7 @@ for i=1:length(tree)-1
         alpha = child{end};
         % Convert copula parameter to Kendall's tau
         tau = copulastat(family, alpha);        
-        labels{count} = sprintf('\\tau=%.3f', tau);
+        labels{count} = sprintf('$%.3f$', tau);
         [nodes, labels, count] = hac2nodes(family, child, nodes, labels, count);        
     else
         % Use variable number as a label of this subtree
