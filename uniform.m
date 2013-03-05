87d064c02030a20af9237496e0f31eff89fb30f3
Martin Scavnicky <martin.scavnicky@gmail.com>
2013-03-05 13:36:25 +0100

    merged rankmax and uniform functions

--- uniform.m
+++ uniform.m
@@ -16,3 +16,32 @@ end
 
 end
 
+function [ R ] = rankmax( X )
+%RANKMAX Returns vector of one-based ranks for each element
+%   For the groups of same element, the maximum rank is returned. This can
+%   be viewed as number of elements smaller or equal than given number.
+
+% Number of elements
+n = size(X, 1);
+% Preallocate ranks vector
+R = zeros(n, 1);
+% Sort the array and retrieve indices
+[S, I] =  sort(X);
+% Rank of the previous element
+r = n;
+% Value of the previous element
+prev = S(n);
+
+for i=n:-1:1
+    x = S(i);
+    if x == prev
+        R(I(i)) = r;
+    else
+        prev = x;
+        r = i;
+        R(I(i)) = r;
+    end    
+end
+
+end
+
