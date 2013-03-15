6cb52bbc867ca1e25f2706dbd7a1400f29a85e1e
Martin Scavnicky <martin.scavnicky@gmail.com>
2013-03-15 19:11:27 +0100

    toclasses now work with list of cut points

--- toclasses.m
+++ toclasses.m
@@ -1,10 +1,16 @@
-function [ C ] = toclasses( Y, c )
+function [ C ] = toclasses( Y, n, cuts )
 %TOCLASSES Converts continuouos attribute into n equal sized classes.
 
-C = zeros(size(Y));
-for i=1:c
-    q = quantile(Y, i/c);
-    C(Y <= q & C == 0) = i;
+% Use quantiles if cuts not specified
+if nargin < 3
+    cuts = quantile(Y, (1:n-1)/n);
+end
+
+cuts = [(min(Y) - 1) cuts (max(Y) + 1)];
+
+C = NaN(size(Y));
+for i=1:n
+    C(Y > cuts(i) & Y <= cuts(i+1)) = i;
 end
 
 end
