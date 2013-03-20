7677f2e7015a548020d3e81f8a5fb470fd4b970a
Martin Scavnicky <martin.scavnicky@gmail.com>
2013-03-20 13:49:06 +0100

    archim cdf function now uses matlab implementation in 2d

--- +archim/cdf.m
+++ +archim/cdf.m
@@ -4,6 +4,13 @@ function [ Y ] = cdf( family, U, alpha )
 
 % Copula dimension is necessary for parameter validation
 d = size(U, 2);
+
+% For 2-dimensional case use existing MATLAB implementation
+if d == 2
+   Y = copulacdf(family, U, alpha);
+   return;
+end
+
 % Get bounds for family and dimension
 [ lowerBound, upperBound ] = archim.bounds(family, d);
 % Verify parameter against bounds
@@ -12,15 +19,6 @@ assert(alpha ~= 0, 'Copula parameter cannot be zero.');
 
 % Compute CDF according to definition
 Y = archim.gen(family, sum(archim.inv(family, U, alpha), 2), alpha);
-
-% For large levels of alpha Frank copula is numerically unstable and
-% produces Inf values. Fot rhis reason infinite elements are replaced by
-% estimation provided by the comonotonicity copula.
-if strcmp(family, 'frank')
-    if sum(isinf(Y)) > 0 && (alpha > 40)
-        Y(isinf(Y)) = min(U(isinf(Y),:),[],2);
-    end
-end
 
 end
 
--- test/test_archim_cdf.m
+++ test/test_archim_cdf.m
@@ -16,4 +16,4 @@ function testArchimCdfAgainstMatlabForHighAlpha
     X = [0.1 0.2; 0.3 0.8; 0.9 0.4; 0.5 0.5; 0.9 0.8];
     assertVectorsAlmostEqual(archim.cdf('clayton', X, 50), copulacdf('clayton', X, 50));
     assertVectorsAlmostEqual(archim.cdf('gumbel', X, 50), copulacdf('gumbel', X, 50));
-    assertVectorsAlmostEqual(archim.cdf('frank', X, 50), copulacdf('frank', X, 50), 'absolute', 1e-3);
+    assertVectorsAlmostEqual(archim.cdf('frank', X, 50), copulacdf('frank', X, 50));
