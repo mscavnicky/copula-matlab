3219da0768c045cf769c714b70673e5dcd918303
Martin Scavnicky <martin.scavnicky@gmail.com>
2013-03-28 16:04:00 +0100

    refactored copula.crossval method for better readability

--- +copula/crossval.m
+++ +copula/crossval.m
@@ -2,16 +2,23 @@ function [ confusionMatrix ] = crossval( family, method, X, Y, k )
 %COPULA.CROSSVAL
 
 dbg('copula.crossval', 2, 'Cross validation for family %s.\n', family);
+
 % Reset random numbe generator to obtain same partitions every run
 rng(42);
-
-% Define classificication function
-C = unique(Y);
-classifyFun = @(X, Y, TX, TY) confusionmat(TY, copula.classify(family, method, TX, X, Y), 'order', C);
-
 % Run stratified k-fold cross-validation
-confusionMatrix = crossval(classifyFun, X, Y, 'kfold', k, 'stratify', Y);
-confusionMatrix = reshape(sum(confusionMatrix), numel(C), numel(C));
+fun = @(X, Y, TX, TY) classifyAndTest(family, method, X, Y, TX, TY);
+confusionMatrix = crossval(fun, X, Y, 'kfold', k, 'stratify', Y);
 
+% Reshape confusion matrix
+numClasses = numel(unique(Y));
+% Perform sum of confusion matrices from all the runs
+confusionMatrix = reshape(sum(confusionMatrix), numClasses, numClasses);
+
+end
+
+function [ confusionMatrix ] = classifyAndTest(family, method, X, Y, TX, TY)
+    K = unique(Y);    
+    TYhat = copula.classify(family, method, TX, X, Y);
+    confusionMatrix = confusionmat(TY, TYhat, 'order', K);
 end
 
