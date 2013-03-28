31508990654b084601aa8459e9daa3fab641cd7f
Martin Scavnicky <martin.scavnicky@gmail.com>
2013-03-28 16:00:14 +0100

    thesis images are now plotted using different font and into pdf

--- copulaimg.m
+++ copulaimg.m
@@ -9,16 +9,20 @@ if (nargin < 5)
     max = 1;
 end
 
+colormap('default');
+
 fig = figure(1);
 set(fig, 'Position', [0, 0, 800, 400]);
 
 sub1 = subplot(1,2,1);
 [U1, U2, Y] = ugrid(fun, 20, min, max);
 surf(U1, U2, Y);
+set(gca, 'FontName', 'NewCenturySchlbk');
 
 sub2 = subplot(1,2,2);
 [U1, U2, Y] = ugrid(fun, 100, min, max);
 contour(U1, U2, Y, clines);
+set(gca, 'FontName', 'NewCenturySchlbk');
 
 set(sub1,'Units','normalized', 'position', [0.05 0.12 0.4 0.8]);
 set(sub2,'Units','normalized', 'position', [0.58 0.17 0.35 0.7]);
@@ -26,7 +30,7 @@ set(sub2,'Units','normalized', 'position', [0.58 0.17 0.35 0.7]);
 set(gcf, 'PaperUnits', 'centimeters');
 set(gcf, 'PaperSize', [13 6.5]);
 set(gcf, 'PaperPosition', [0 0 13 6.5]);
-print('-dpng', '-r300', fname);
+print('-dpdf', '-r300', fname);
 
 end
 
--- thesis.m
+++ thesis.m
@@ -1,26 +1,28 @@
-%% Independence copula
-copulaimg(@(U) prod(U, 2), '../Images/independence-cdf.png');
+%% Copula densities
 
-%% Comonotonicity copula
-copulaimg(@(U) min(U, [], 2), '../Images/comonotonicity-cdf.png');
+% Independence copula
+copulaimg(@(U) prod(U, 2), '../Images/independence-cdf.pdf');
 
-%% Countermonotonicity copula
-copulaimg(@(U) max(sum(U, 2) - 1, 0), '../Images/countermonotonicity-cdf.png');
+% Comonotonicity copula
+copulaimg(@(U) min(U, [], 2), '../Images/comonotonicity-cdf.pdf');
 
-%% Guassian copula
-copulaimg(@(U) copulapdf('gaussian', U, 0.3), '../Images/gaussian-pdf.png', 20, 0.05, 0.95);
+% Countermonotonicity copula
+copulaimg(@(U) max(sum(U, 2) - 1, 0), '../Images/countermonotonicity-cdf.pdf');
 
-%% Student-t copula
-copulaimg(@(U) copulapdf('t', U, 0.3, 3), '../Images/student-pdf.png', 20, 0.05, 0.95);
+% Guassian copula
+copulaimg(@(U) copulapdf('gaussian', U, 0.3), '../Images/gaussian-pdf.pdf', 20, 0.05, 0.95);
 
-%% Clayton copula
-copulaimg(@(U) copulapdf('clayton', U, 0.7), '../Images/clayton-pdf.png', 30, 0.05, 0.95);
+% Student-t copula
+copulaimg(@(U) copulapdf('t', U, 0.3, 3), '../Images/student-pdf.pdf', 20, 0.05, 0.95);
 
-%% Gumbel copula
-copulaimg(@(U) copulapdf('gumbel', U, 1.3), '../Images/gumbel-pdf.png', 20, 0.05, 0.95);
+% Clayton copula
+copulaimg(@(U) copulapdf('clayton', U, 0.7), '../Images/clayton-pdf.pdf', 30, 0.05, 0.95);
 
-%% Frank copula
-copulaimg(@(U) copulapdf('frank', U, -2), '../Images/frank-pdf.png', 20, 0.05, 0.95);
+% Gumbel copula
+copulaimg(@(U) copulapdf('gumbel', U, 1.3), '../Images/gumbel-pdf.pdf', 20, 0.05, 0.95);
+
+% Frank copula
+copulaimg(@(U) copulapdf('frank', U, -2), '../Images/frank-pdf.pdf', 20, 0.05, 0.95);
 
 %% Positive and negative dependency scatter
 
