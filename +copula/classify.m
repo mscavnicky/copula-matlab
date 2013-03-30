2a177aaf675a752834410cb7844b670883db424d
Martin Scavnicky <martin.scavnicky@gmail.com>
2013-03-30 16:12:56 +0100

    fixed classification bug

--- +copula/classify.m
+++ +copula/classify.m
@@ -1,8 +1,9 @@
 function [ TY ] = classify( family, method, TX, X, Y )
 %COPULA.CLASSIFY 
 
-n = size(Y, 1);
-d = size(X, 2);
+[n, d] = size(X);
+% Size of the test dataset
+t = size(TX, 1);
 
 % Obtain list of classes
 K = unique(Y);
@@ -13,16 +14,19 @@ numClasses = numel(K);
 dbg('copulas.classify', 3, 'Fitting copulas for class.\n');
 for i=1:numClasses
     % Prior class probability
-    C(i).P = sum(Y == K(i)) / n;
+    C(i).Prior = sum(Y == K(i)) / n;
     % Training data for class i
     C(i).X = X(Y == K(i), :);
+    % Test data for class i
+    C(i).TX = TX;
         
     % If model requires preprocessing do it for both train and test dataset
     family_i = family;
     if ismember(family, {'claytonhac*', 'gumbelhac*', 'frankhac*'})
         P = hac.preprocess( family, C(i).X, method );
         C(i).X = C(i).X * P;
-        TX = TX * P;
+        % Keep the preprocess test data
+        C(i).TX = TX * P;
         family_i = family(1:end-1);
     end
     
@@ -40,19 +44,19 @@ for i=1:numClasses
 end
 
 % Compute likelihood for test sample in each copula
-L = zeros(size(TX, 1), d + 2, numClasses);
+L = zeros(t, d + 2, numClasses);
 for i=1:numClasses
     dbg('copulas.classify', 3, 'Computing likelihood for class %d.\n', i);
     if strcmp(method, 'CML')
-        L(:,1,i) = copula.pdf(C(i).Copula, empcdf(C(i).X, TX));
-        L(:,2:d+1,i) = emppdf(C(i).X, TX);
+        L(:,1,i) = copula.pdf(C(i).Copula, empcdf(C(i).X, C(i).TX));
+        L(:,2:d+1,i) = emppdf(C(i).X, C(i).TX);
     elseif strcmp(method, 'IFM')
-        L(:,1,i) = copula.pdf(C(i).Copula, pit(TX, {C(i).Margins.ProbDist}));
-        L(:,2:d+1,i) = problike(TX, {C(i).Margins.ProbDist});
+        L(:,1,i) = copula.pdf(C(i).Copula, pit(C(i).TX, {C(i).Margins.ProbDist}));
+        L(:,2:d+1,i) = problike(C(i).TX, {C(i).Margins.ProbDist});
     else
         error('Unknown method %s', method);
     end
-    L(:,d+2,i) = C(i).P;
+    L(:,d+2,i) = C(i).Prior;
 end
 
 % Make a product of likelihoods and make a matrix out of it
@@ -61,9 +65,9 @@ PL = PL(:, :);
 
 % For each sample choose class with the highest likelihood and if they are
 % same use highest copula likelihood, otherwise choose randomly
-TY = zeros(size(TX, 1), 1);
+TY = zeros(t, 1);
 
-for i=1:size(TX, 1)
+for i=1:t
     maxIndices = allmax(PL(i, :));
     if numel(maxIndices) == 1
         TY(i) = maxIndices;
