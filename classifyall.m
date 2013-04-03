002efd6f4d6729e7b43762776d307fc6e571927f
Martin Scavnicky <martin.scavnicky@gmail.com>
2013-04-03 15:18:24 +0200

    merged crossval function to classifyall function

--- classifyall.m
+++ classifyall.m
@@ -25,12 +25,36 @@ end
 end
 
 function [ result ] = classifyUsing( family, method, X, Y )
-    confus = copula.crossval(family, method, X, Y, 10);
+    confus = copcrossval(family, method, X, Y, 10);
     
     result.Family = family;
     result.Method = method;
     result.Confus = confus;
     result.Correct = sum(confus(eye(size(confus)) == 1));
     result.Incorrect = sum(confus(eye(size(confus)) == 0));
+end
+
+function [ confusionMatrix ] = copcrossval( family, method, X, Y, k )
+%COPULA.CROSSVAL
+
+dbg('copula.crossval', 2, 'Cross validation for family %s.\n', family);
+
+% Reset random numbe generator to obtain same partitions every run
+rng(42);
+% Run stratified k-fold cross-validation
+fun = @(X, Y, TX, TY) classifyAndTest(family, method, X, Y, TX, TY);
+confusionMatrix = crossval(fun, X, Y, 'kfold', k, 'stratify', Y);
+
+% Reshape confusion matrix
+numClasses = numel(unique(Y));
+% Perform sum of confusion matrices from all the runs
+confusionMatrix = reshape(sum(confusionMatrix), numClasses, numClasses);
+
+end
+
+function [ confusionMatrix ] = classifyAndTest(family, method, X, Y, TX, TY)
+    K = unique(Y);    
+    TYhat = copula.classify(family, method, TX, X, Y);
+    confusionMatrix = confusionmat(TY, TYhat, 'order', K);
 end
 
